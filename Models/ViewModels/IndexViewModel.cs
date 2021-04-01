651f76c1786c8e7b407d434e69de0000459c1622
annikadeon <66037631+annikadeon@users.noreply.github.com>
2021-03-31 20:41:23 -0600

    comments added

--- Models/ViewModels/IndexViewModel.cs
+++ Models/ViewModels/IndexViewModel.cs
@@ -7,6 +7,7 @@ namespace Assignment10.Models.ViewModels
 {
     public class IndexViewModel
     {
+        //to hold info for the index page
         public List<Bowlers> Bowlers{get; set;}
         public PageNumberInfo PageNumberInfo { get; set; }
         public string TeamCat { get; set; }
